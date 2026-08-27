680960330cdc6983ab722bce7e1d36866517c3f9
ashvinijangid <ashvinijangid@gmail.com>
2026-08-27 17:47:23 +0530

    Update flutter_bootstrap service worker version

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3080926059" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3129238506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine({
